e957e17f4b50c184d5bfa56e16d48f80eaf9efb1
Arun Nair <arunnair018@gmail.com>
2020-07-12 20:19:53 +0530

    updated routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ const service = require("../services");
 // Router
 module.exports = (app) => {
   // api endpoint for normal year
-  app.route("/schedule/v1/year").post(service.schedule);
+  app.route("/api/v1/schedule/year").post(service.schedule);
   // api endpoint for leap year
-  app.route("/schedule/v1/leapyear").post(service.scheduleLeap);
+  app.route("/api/v1/schedule/leapyear").post(service.scheduleLeap);
 };
